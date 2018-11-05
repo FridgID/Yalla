b2fc930f5d1713387d4ff53262b4e141dfd928b4
ElvisDot <elvis.doettinger@gmail.com>
2018-11-05 16:59:04 +0100

    Add some Tour seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "seeding..."
+
+#TODO: date/time stuff
+#TODO: users
+
+Tour.create([
+  {
+    title: "fancy food in ams",
+    capacity: 6,
+    category: "food",
+    longitude: 52.376189,
+    latitude: 4.893036,
+    location: "amsterdam",
+    price_euro: 5
+  },
+  {
+    title: "buy beer",
+    capacity: 200,
+    category: "beer",
+    longitude: 52.376199,
+    latitude: 4.893236,
+    location: "amsterdam",
+    price_euro: 100
+  },
+  {
+    title: "bike & beer",
+    capacity: 10,
+    category: "bike",
+    longitude: 52.376198,
+    latitude: 4.893237,
+    location: "amsterdam",
+    price_euro: 20
+  },
+])
