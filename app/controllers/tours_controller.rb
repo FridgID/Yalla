782e559f1865b8c82a6aa69e1f890641e8c09c8c
Mary Healy <maryhealy1978@gmail.com>
2018-11-05 17:02:56 +0100

    Add tours controller methods

---
+++ app/controllers/tours_controller.rb
@@ -0,0 +1,25 @@
+class ToursController < ApplicationController
+
+  def index
+    @tours = Tour.all
+  end
+
+  def show
+    @tour = tour.find(params[:id])
+
+  end
+  def new
+    @tour = Tour.new
+  end
+
+  def create
+    @tour = Tour.new(tour_params)
+  end
+
+  private
+
+  def tour_params
+    params.require(:tour).permit(:title, :capacity, :category, :longitude, :latitude, :start_time, :end_time, :date, :location, :price_euro, :user_id)
+  end
+end
+
