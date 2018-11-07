3f67f68d0c3720c875394df42fafc19252258b22
Karin Lilliehöök <karin@lilliehooks.se>
2018-11-07 10:55:02 +0100

    add photo column

--- db/schema.rb
+++ db/schema.rb
@@ -63,7 +63,6 @@ ActiveRecord::Schema.define(version: 2018_11_06_150803) do
     t.datetime "updated_at", null: false
     t.string "name", default: "", null: false
     t.string "photo"
-
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
