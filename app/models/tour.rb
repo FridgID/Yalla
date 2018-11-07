d981d2f2a757da37ab2c232966a0d25dae9bafdf
ElvisDot <elvis.doettinger@gmail.com>
2018-11-07 12:06:55 +0100

    Add icon classes to category hash

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,6 +1,13 @@
 class Tour < ApplicationRecord
   belongs_to :user
 
-  CATEGORYS=["walk", "food", "beer", "boat", "bike"]
-  validates :category, inclusion: { in: CATEGORYS }
+  # CATEGORYS=["walk", "food", "beer", "boat", "bike"] # old
+  CATEGORYS = {
+    walk: "fas fa-walking",
+    food: "fas fa-beer",
+    beer: "fas fa-bicycle",
+    boat: "fas fa-ship",
+    bike: "fas fa-utensils"
+  }
+  validates :category, inclusion: { in: CATEGORYS.keys.map(&:to_s) }
 end
