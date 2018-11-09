dde9d6231d7e41971949d4519641cfc1336c1a97
Mary Healy <maryhealy1978@gmail.com>
2018-11-09 15:36:19 +0100

    fix seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ User.create!(name: "funnybunny", email: '1@2.nl', password: 'password')
 User.create!(name: "lolbob", email: '2@2.nl', password: 'password')
 
 10.times do |x|
-  User.create!(name: "#{Faker::Artist.name}#{Random.rand(0..2050)}", email: "#{x}@1.nl", password: 'password')
+  User.create!(name: "#{Faker::TheFreshPrinceOfBelAir.character}#{Random.rand(0..2050)}", email: "#{x}@1.nl", password: 'password')
 end
 
 user_ids = (User.first.id..User.last.id).to_a
@@ -26,156 +26,156 @@ puts "create tours..."
 
 Tour.create!([
   {
-    title: "fancy food in ams",
+    title: "FANCY FOOD TRAIL",
     description: "Really good food trail around Amsterdam",
     capacity: 6,
     category: "food",
     longitude: 4.893036,
     latitude: 52.376189,
-    location: "amsterdam",
+    location: "Amsterdam",
     price_euro: 5,
     user_id: user_ids.sample
   },
   {
-    title: "Eat sleep food repeat",
-    description: "48 1 on 1 food marathon.",
+    title: "Albert Cyup Straat",
+    description: "Enjoy the culinary delights of Albert Cyup Straat.",
     capacity: 2,
     category: "food",
     longitude: 4.893036,
     latitude: 52.376129,
-    location: "amsterdam",
+    location: "Amsterdam",
     price_euro: 10,
     user_id: user_ids.sample
   },
   {
-    title: "free food for money",
-    description: "Its actually free if you forget about the money you pay.",
-    capacity: 4,
-    category: "food",
+    title: "Canal Cruise",
+    description: "Take a canal cruise through the historice Amsterdam canals",
+    capacity: 10,
+    category: "boat",
     longitude: 4.893039,
     latitude: 52.376289,
-    location: "amsterdam",
-    price_euro: 500,
+    location: "Amsterdam",
+    price_euro: 50,
     user_id: user_ids.sample
   },
   {
-    title: "free beer (tap water)",
-    description: "Water? Beer? Difference?     FREE!!!!!!!!!!!",
-    capacity: 90,
+    title: "Craft Beer Rub",
+    description: "5km run to five craft breweries in Amsterdam. We start at De Praal in the Wallen",
+    capacity: 20,
     category: "beer",
     longitude: 4.893036,
     latitude: 52.376182,
-    location: "amsterdam",
-    price_euro: 0,
+    location: "Amsterdam",
+    price_euro: 20,
     user_id: user_ids.sample
   },
   {
-    title: "friday drinks",
-    description: "Every day free drinks for only 0 euro. (You have to bring them yourself)",
-    capacity: 9999,
+    title: "Beer & Bike",
+    description: "Take a cycle to the craft breweries in Amsterdam. We will take the ferry Noord to reknown Oedipus brewery.",
+    capacity: 10,
     category: "beer",
     longitude: 4.893036,
     latitude: 52.376189,
-    location: "amsterdam",
-    price_euro: 0,
+    location: "Amsterdam",
+    price_euro: 10,
     user_id: user_ids.sample
   },
   {
-    title: "buy beer",
-    description: "Really good beer trail around Amsterdam",
-    capacity: 200,
-    category: "beer",
+    title: "Boat to Ouderkerk",
+    description: "Take our boat down the Amstel to the lovely village of Ouderkerk and enjoy your lunch there or picnic on the side of the Amstel",
+    capacity: 6,
+    category: "boat",
     longitude: 4.893236,
     latitude: 52.376199,
-    location: "amsterdam",
-    price_euro: 100,
+    location: "Amsterdam",
+    price_euro: 10,
     user_id: user_ids.sample
   },
   {
-    title: "bike & beer",
-    description: "Really good bike & beer trail around Amsterdam",
+    title: "Bike To Marken",
+    description: "Join me to a long cycle to the beautiful village of Marken",
     capacity: 10,
     category: "bike",
     longitude: 4.893237,
     latitude: 52.376198,
-    location: "amsterdam",
+    location: "Amsterdam",
     price_euro: 20,
     user_id: user_ids.sample
   },
   {
-    title: "Paris foo bar",
-    description: "Some developers eating beers",
-    capacity: 2048,
+    title: "Food Trail in Jordaan",
+    description: "Join me to enjoy the cuilnary delights of the Jordaan.",
+    capacity: 15,
     category: "food",
     longitude: 2.2945,
     latitude: 48.8582,
-    location: "paris",
+    location: "Amsterdam",
     price_euro: 80,
     user_id: user_ids.sample
   },
   {
-    title: "chill boat tour",
-    description: "Some nice shippy bippy in ze river",
+    title: "Night Boat Tour",
+    description: "Come and join me as we boat around the world famous canals at night",
     capacity: 10,
     category: "boat",
     longitude: 52.376198,
     latitude: 4.893237,
-    location: "amsterdam",
-    price_euro: 20,
+    location: "Amsterdam",
+    price_euro: 50,
     user_id: user_ids.sample
   },
   {
-    title: "Walk and talk",
-    description: "Sniff some fresh air in the forests",
+    title: "Jenveer Tour",
+    description: "Join me in sipping the famous Jenveer tipple",
     capacity: 15,
-    category: "walk",
+    category: "food",
     longitude: 52.376182,
     latitude: 4.893267,
-    location: "amsterdam",
+    location: "Amsterdam",
     price_euro: 1,
     user_id: user_ids.sample
   },
   {
-    title: "Hidden places in amsterdam",
+    title: "Hidden Places In Amsterdam",
     description: "Feel the power of an underground tour guide that shows some magic.",
     capacity: 8,
     category: "walk",
     longitude: 52.376281,
     latitude: 4.893267,
-    location: "amsterdam",
-    price_euro: 77,
+    location: "Amsterdam",
+    price_euro: 25,
     user_id: user_ids.sample
   },
   {
-    title: "You are a pirate!",
-    description: "Do what you want cause you are free!! Arr!",
+    title: "Tour the Pijp",
+    description: "Join me on a walkig tour of the Pijp. We will take in the famous Albert Cyup street",
     capacity: 8,
-    category: "boat",
+    category: "walk",
     longitude: 52.376281,
     latitude: 4.893268,
-    location: "amsterdam",
-    price_euro: 0,
+    location: "Amsterdam",
+    price_euro: 20,
     user_id: user_ids.sample
   },
   {
-    title: "Bee biking!",
-    description: "We dress as bees and cycle some bikes :D",
-    capacity: 22,
+    title: "Bike to Ouderkerk",
+    description: "Cycle along the Amstel to the lovely village of Ouderkerk",
+    capacity: 15,
     category: "bike",
     longitude: 52.376261,
     latitude: 4.893268,
-    location: "amsterdam",
-    price_euro: 1,
+    location: "Amsterdam",
+    price_euro: 10,
     user_id: user_ids.sample
   },
   {
-    title: "Jungle adventure in ams",
-    description: "Snakes and dirt",
-    capacity: 8,
+    title: "Walk in the Bos",
+    description: "Join me a gorgeous walk around the Bos of the Amsterdam",
+    capacity: 10,
     category: "walk",
     longitude: 4.893167,
     latitude: 52.376182,
-    location: "amsterdam",
+    location: "Amsterdam",
     price_euro: 22,
     user_id: user_ids.sample
   }
@@ -191,7 +191,7 @@ r_count.times do
   Review.create!([
     {
       rating: Random.rand(0..5),
-      comment: Faker::Hacker.say_something_smart,
+      comment: Faker::TheFreshPrinceOfBelAir.quote,
       tour_id: tour_ids.sample,
       user_id: user_ids.sample
     }
